ae708fa94d21c014125c1abe2f500f0598e6c657
TarkWight <arsentiy.gonda@gmail.com>
2025-11-12 19:33:39 +0700

    * upgrade main.rs to async Axum web server
    Replaced println! with minimal Axum server (GET / route).
    Prepared project for further REST API development.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listner = TcpListener::bind("127.0.0.1:8080")
+        .await
+        .expect("bind failed");
+
+    axum::serve(listner, app)
+        .await
+        .expect("server error");
+
+}
+
+async fn root() -> &'static str {
+    "Reverse Gantt API"
 }
