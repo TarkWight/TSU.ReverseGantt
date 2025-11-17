083e8b76d43348d383183fa294dd7a8da51438e3
TarkWight <arsentiy.gonda@gmail.com>
2025-11-17 10:42:28 +0700

    refactor: add line breaks

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -52,8 +52,10 @@ async fn shutdown_signal() {
     {
         use tokio::signal::unix::{signal, SignalKind};
 
-        let mut sigterm = signal(SignalKind::terminate()).expect("failed to install SIGTERM handler");
-        let mut sigint  = signal(SignalKind::interrupt()).expect("failed to install SIGINT handler");
+        let mut sigterm =
+            signal(SignalKind::terminate()).expect("failed to install SIGTERM handler");
+        let mut sigint =
+            signal(SignalKind::interrupt()).expect("failed to install SIGINT handler");
 
         tokio::select! {
             _ = sigterm.recv() => {
