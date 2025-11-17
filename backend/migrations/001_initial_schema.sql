ba3524f9899ada2e36025f0efa7457136c937d8a
TarkWight <arsentiy.gonda@gmail.com>
2025-11-17 11:26:19 +0700

    + migrations: add projects table
    Added table 'projects' with timestamps and uuid_generate_v4() PK

--- backend/migrations/001_initial_schema.sql
+++ backend/migrations/001_initial_schema.sql
@@ -1,1 +1,11 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE projects (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    description TEXT,
+    start_date DATE,
+    due_date DATE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
