c6e7808bc1d62c06979f89ba85d1d963c39f0649
TarkWight <arsentiy.gonda@gmail.com>
2025-11-17 09:46:27 +0700

    + config: add top-level Config struct
    Add Config struct containing server: ServerConfig and database: DatabaseConfig

--- backend/src/config/mod.rs
+++ backend/src/config/mod.rs
@@ -1,6 +1,12 @@
 use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    pub server: ServerConfig,
+    pub database: DatabaseConfig,
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct ServerConfig {
     pub host: String,
     pub port: u16,
