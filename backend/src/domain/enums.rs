3cb2194e252a98b021a0dd22e7372dd21aded934
TarkWight <arsentiy.gonda@gmail.com>
2025-11-20 15:23:44 +0700

    * domain: implement Display for DepType
    Add string representation for DepType

--- backend/src/domain/enums.rs
+++ backend/src/domain/enums.rs
@@ -144,3 +144,14 @@ pub enum DepType {
     SS, // Start-to-Start
     SF, // Start-to-Finish
 }
+
+impl std::fmt::Display for DepType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DepType::FS => write!(f, "FS"),
+            DepType::FF => write!(f, "FF"),
+            DepType::SS => write!(f, "SS"),
+            DepType::SF => write!(f, "SF"),
+        }
+    }
+}
