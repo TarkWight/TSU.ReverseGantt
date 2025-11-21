0a4912fdc7aa160d184cc37c6b64c7c901875761
TarkWight <arsentiy.gonda@gmail.com>
2025-11-21 12:51:44 +0700

    * services/project: implement get_all()
    Implement get_all() using SQLx query to load all projects

--- backend/src/services/project_service.rs
+++ backend/src/services/project_service.rs
@@ -1,4 +1,4 @@
-use crate::utils::{AppResult, Id};
+use crate::utils::{AppError, AppResult, Id};
 use crate::domain::Project;
 use async_trait::async_trait;
 use sqlx::PgPool;
@@ -24,7 +24,31 @@ impl ProjectServiceImpl {
 #[async_trait]
 impl ProjectService for ProjectServiceImpl {
     async fn get_all(&self) -> AppResult<Vec<Project>> {
-        todo!()
+        let rows = sqlx::query!(
+        r#"
+        SELECT
+            id, name, description,
+            start_date, due_date,
+            created_at, updated_at
+        FROM projects
+        ORDER BY created_at DESC
+        "#
+    )
+            .fetch_all(&self.pool)
+            .await
+            .map_err(|e| AppError::Internal(anyhow::anyhow!(e.to_string())))?;
+
+        let projects = rows.into_iter().map(|row| Project {
+            id: row.id,
+            name: row.name,
+            description: row.description,
+            start_date: row.start_date,
+            due_date: row.due_date,
+            created_at: row.created_at,
+            updated_at: row.updated_at,
+        }).collect();
+
+        Ok(projects)
     }
 
     async fn get_by_id(&self, id: Id) -> AppResult<Project> {
