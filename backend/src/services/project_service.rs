b6026bd0deb7a8a03fc7bc66351f26a2b6457ffa
TarkWight <arsentiy.gonda@gmail.com>
2025-11-21 12:34:24 +0700

    + services: add ProjectService trait
    Add base ProjectService trait with CRUD method signatures using AppResult

---
+++ backend/src/services/project_service.rs
@@ -0,0 +1,12 @@
+use crate::utils::{AppResult, Id};
+use crate::domain::Project;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait ProjectService: Send + Sync {
+    async fn get_all(&self) -> AppResult<Vec<Project>>;
+    async fn get_by_id(&self, id: Id) -> AppResult<Project>;
+    async fn create(&self, project: Project) -> AppResult<Project>;
+    async fn update(&self, id: Id, project: Project) -> AppResult<Project>;
+    async fn delete(&self, id: Id) -> AppResult<()>;
+}
