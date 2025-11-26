d37fb8fa603845fed370c587f8877bc9eb271aac
TarkWight <arsentiy.gonda@gmail.com>
2025-11-26 15:15:41 +0700

    * services/schedule: implement reverse scheduling algorithm
    Add full LS/LF backward-pass computation supporting FS/SS/SF/FF dependencies

--- backend/src/services/schedule_service.rs
+++ backend/src/services/schedule_service.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
 use sqlx::query;
+use chrono::{Duration};
+
 use crate::utils::{AppResult, AppError, Id};
 use crate::domain::{
     Task,
@@ -105,5 +107,113 @@ impl ScheduleServiceImpl {
         }).collect();
 
         Ok((domain_tasks, domain_deps))
+    }
+
+    fn compute_reverse_schedule(
+        &self,
+        tasks: &mut [Task],
+        dependencies: &[Dependency],
+        project_due_date: chrono::NaiveDate,
+    ) -> AppResult<()> {
+        if tasks.is_empty() {
+            return Ok(());
+        }
+
+        use std::collections::{VecDeque, HashMap};
+
+        let mut successors: HashMap<Id, Vec<&Dependency>> = HashMap::new();
+        let mut predecessors: HashMap<Id, Vec<&Dependency>> = HashMap::new();
+
+        for dep in dependencies {
+            successors.entry(dep.from_task_id).or_default().push(dep);
+            predecessors.entry(dep.to_task_id).or_default().push(dep);
+        }
+
+        let mut index: HashMap<Id, usize> = HashMap::new();
+        for (i, t) in tasks.iter().enumerate() {
+            index.insert(t.id, i);
+        }
+
+        let deadline = project_due_date.and_hms_opt(23, 59, 59).unwrap().and_utc();
+
+        let mut queue = VecDeque::new();
+        for t in tasks.iter() {
+            if !successors.contains_key(&t.id) {
+                queue.push_back(t.id);
+            }
+        }
+
+        while let Some(task_id) = queue.pop_front() {
+            let idx = index[&task_id];
+            let task = &mut tasks[idx];
+
+            if task.schedule.lf.is_none() {
+                task.schedule.lf = Some(deadline);
+
+                if let Some(dur) = task.estimated_duration {
+                    task.schedule.ls = task.schedule.lf.map(|lf| lf - Duration::seconds(dur));
+                } else {
+                    task.schedule.ls = task.schedule.lf;
+                }
+            }
+
+            let mut to_update = vec![];
+
+            if let Some(preds) = predecessors.get(&task_id) {
+                let ls = task.schedule.ls;
+                let lf = task.schedule.lf;
+
+                for dep in preds {
+                    let pred_idx = index[&dep.from_task_id];
+                    let pred_task = &tasks[pred_idx];
+
+                    let candidate_lf = match dep.dep_type {
+                        DepType::FS => ls.map(|ls| ls - Duration::seconds(dep.min_gap)),
+                        DepType::FF => lf.map(|lf| lf - Duration::seconds(dep.min_gap)),
+                        DepType::SS => ls.map(|ls| {
+                            let pred_dur = pred_task.estimated_duration.unwrap_or(0);
+                            ls - Duration::seconds(pred_dur + dep.min_gap)
+                        }),
+                        DepType::SF => lf.map(|lf| lf - Duration::seconds(dep.min_gap)),
+                    };
+
+                    if let Some(new_lf) = candidate_lf {
+                        let should =
+                            pred_task.schedule.lf.map(|old| new_lf < old).unwrap_or(true);
+                        if should {
+                            to_update.push((dep.from_task_id, new_lf));
+                        }
+                    }
+                }
+            }
+
+            for (pid, new_lf) in to_update {
+                let idx = index[&pid];
+                let pred = &mut tasks[idx];
+                pred.schedule.lf = Some(new_lf);
+
+                if let Some(dur) = pred.estimated_duration {
+                    pred.schedule.ls = Some(new_lf - Duration::seconds(dur));
+                } else {
+                    pred.schedule.ls = Some(new_lf);
+                }
+
+                queue.push_back(pid);
+            }
+        }
+
+        for t in tasks.iter_mut() {
+            if let (Some(ls), Some(lf)) = (t.schedule.ls, t.schedule.lf) {
+                if let Some(dur) = t.estimated_duration {
+                    let es = ls;
+                    let ef = es + Duration::seconds(dur);
+                    let slack = (lf - ef).num_seconds();
+                    t.schedule.slack = Some(slack);
+                    t.schedule.is_critical = slack == 0;
+                }
+            }
+        }
+
+        Ok(())
     }
 }
