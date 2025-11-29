2ee650141872d4419378a4c1ee169db50fa5362d
TarkWight <arsentiy.gonda@gmail.com>
2025-11-29 14:50:34 +0700

    + services: add ReviewServiceImpl stub
    Add initial ReviewServiceImpl implementation with stubbed methods returning internal errors

--- backend/src/services/review_service.rs
+++ backend/src/services/review_service.rs
@@ -2,6 +2,7 @@ use async_trait::async_trait;
 use sqlx::PgPool;
 use crate::domain::Review;
 use crate::utils::{AppResult, Id};
+use crate::utils::AppError;
 
 #[async_trait]
 pub trait ReviewService: Send + Sync {
@@ -17,5 +18,26 @@ pub struct ReviewServiceImpl {
 impl ReviewServiceImpl {
     pub fn new(pool: PgPool) -> Self {
         Self { pool }
+    }
+}
+
+#[async_trait]
+impl ReviewService for ReviewServiceImpl {
+    async fn get_by_task(&self, _task_id: Id) -> AppResult<Option<Review>> {
+        Err(AppError::Internal(anyhow::anyhow!(
+            "get_by_task not implemented yet"
+        )))
+    }
+
+    async fn create(&self, _review: Review) -> AppResult<Review> {
+        Err(AppError::Internal(anyhow::anyhow!(
+            "create not implemented yet"
+        )))
+    }
+
+    async fn update(&self, _id: Id, _review: Review) -> AppResult<Review> {
+        Err(AppError::Internal(anyhow::anyhow!(
+            "update not implemented yet"
+        )))
     }
 }
