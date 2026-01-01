2f7c584a33b90a551bc3936f2d41c70552e36676
TarkWight <arsentiy.gonda@gmail.com>
2026-01-01 23:16:25 +0700

    + add notification entity

---
+++ backend/src/domain/entities/mod.rs
@@ -0,0 +1,24 @@
+mod artifact;
+mod assignment;
+mod dependency;
+mod membership;
+mod notification;
+mod project;
+mod review;
+mod risk;
+mod schedule;
+mod task;
+mod user;
+
+pub use artifact::*;
+pub use assignment::*;
+pub use dependency::*;
+pub use membership::*;
+pub use notification::*;
+pub use project::*;
+pub use review::*;
+pub use risk::*;
+pub use schedule::*;
+pub use task::*;
+pub use user::*;
+
