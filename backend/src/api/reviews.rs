64f73a43226d3cac0c362e2745f85e1e2f82ec8e
TarkWight <arsentiy.gonda@gmail.com>
2026-01-01 23:04:46 +0700

    Exports review API elements
    Exports review request and response models and associated handler functions for external use.

--- backend/src/api/reviews.rs
+++ backend/src/api/reviews.rs
@@ -1,26 +1,3 @@
-use axum::{
-    extract::{Path, State},
-    http::StatusCode,
-    response::IntoResponse,
-    Json,
-};
-use serde::{Deserialize, Serialize};
-use crate::domain::{Review, ReviewDecision};
-use crate::services::ReviewService;
-use crate::utils::{AppResult, parse_id};
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct CreateReviewRequest {
-    pub reviewer_id: String,
-    pub decision: Option<ReviewDecision>,
-    pub comment: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct UpdateReviewRequest {
-    pub decision: Option<ReviewDecision>,
-    pub comment: Option<String>,
-}
-
+pub use crate::api::requests::CreateReviewRequest;
+pub use crate::api::models::ReviewResponse;
+pub use crate::api::handlers::reviews::{get_review, create_review};
