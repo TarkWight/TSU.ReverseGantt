b43a3d39dcfac2175983cb580363f238cd54f449
TarkWight <arsentiy.gonda@gmail.com>
2026-01-07 16:40:44 +0700

    * infra/repo/membership_repo: add `find by project and user` implement

--- backend/src/infra/repositories/membership_repo.rs
+++ backend/src/infra/repositories/membership_repo.rs
@@ -84,7 +84,25 @@ impl MembershipRepository for PgMembershipRepository {
     }
 
     async fn find_by_project_and_user(&self, project_id: Id, user_id: Id) -> anyhow::Result<Option<Membership>> {
-        todo!()
+        let row = sqlx::query!(
+            r#"
+            SELECT id, project_id, user_id, is_leader, tags
+            FROM memberships WHERE project_id = $1 AND user_id = $2
+            "#,
+            project_id,
+            user_id
+        )
+            .fetch_optional(&self.pool)
+            .await
+            .context("Failed to fetch membership")?;
+
+        Ok(row.map(|r| Membership {
+            id: r.id,
+            project_id: r.project_id,
+            user_id: r.user_id,
+            is_leader: r.is_leader,
+            tags: Self::parse_tags(r.tags),
+        }))
     }
 
     async fn find_by_id(&self, id: Id) -> anyhow::Result<Option<Membership>> {
